f0f2eb9123746342443d6de89df882ae8795da52
YesidArboleda <yesison714@gmail.com>
2020-03-23 16:05:28 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, sexo, create_at) VALUES(1, 'Andres', 'masculino', '2017-08-28');
+INSERT INTO clientes (id, nombre, sexo, create_at) VALUES(2, 'John', 'masculino', '2017-08-28');
